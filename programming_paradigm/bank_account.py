9c263146ab76482bd090375e26c92c1963a0e6ad
jamiu <amurejamiu8@gmail.com>
2025-10-04 22:26:38 +0100

    food

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -8,7 +8,10 @@ class BankAccount:
     def withdraw(self, amount):
         if amount <= self.account_balance:
             self.account_balance -= amount
+            print(f"Withdrew: ${amount}")
+            return True
         else:
+            print("Insufficient funds.")
             return False
         
     def display_balance(self):
