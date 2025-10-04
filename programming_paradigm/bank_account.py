7bc480ad79158f4157793fa5a392d6c3edacadc4
jamiu <amurejamiu8@gmail.com>
2025-10-04 22:13:37 +0100

    food

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -12,6 +12,6 @@ class BankAccount:
             return False
         
     def display_balance(self):
-        print(self.account_balance)
+        print(f"Current Balance: {self.account_balance}")
 
     
