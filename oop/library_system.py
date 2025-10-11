e80c42fe841346b1f6fcb24a8e9c2bada80594b0
jamiu <amurejamiu8@gmail.com>
2025-10-11 21:58:58 +0100

    good

--- oop/library_system.py
+++ oop/library_system.py
@@ -12,7 +12,7 @@ class EBook(Book):
         self.file_size = file_size
 
     def __repr__(self):
-        return f"Ebook: {self.title} by {self.author}, file size: {self.file_size}KB"
+        return f"Ebook: {self.title} by {self.author}, File size: {self.file_size}KB"
         
 class PrintBook(Book):
     def __init__(self, title: str, author: str, page_count: int):
@@ -20,7 +20,7 @@ class PrintBook(Book):
         self.page_count = page_count
 
     def __repr__(self):
-        return f"PrintBook: {self.title} by {self.author}, page count: {self.page_count}"
+        return f"PrintBook: {self.title} by {self.author}, Page count: {self.page_count}"
 
 class Library():
     def __init__(self):
